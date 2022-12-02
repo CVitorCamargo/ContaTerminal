191af67e099983c9736c9fba1428e43d07c5e6ea
CVitorCamargo <vitor.camargo@msn.com>
2022-12-01 21:49:12 -0300

    Finalizado experiencia de como ultilizar o scanner em java.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    int numero;
+    String agencia;
+    String nomeCliente;
+    Float saldo;
+
+    Scanner scaner = new Scanner(System.in);
+
+    System.out.println("Por favor, digite o número da Agência !");
+    agencia = scaner.next();
+    nomeCliente = scaner.next();
+    numero = scaner.nextInt();
+    saldo = scaner.nextFloat();
+
+    System.out.println("\"Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +agencia+ ", conta "+numero + "e seu saldo "+saldo+" já está disponível para saque\".");
+
+
     }
+
 }
